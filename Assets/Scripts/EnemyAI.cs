14f720e146465da5819ee2becfa37fbbaf41ce39
keleciaa <1028416@student.sae.edu.au>
2023-10-30 21:56:53 +0800

    enemy dies on click - health system

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Enemy_AI : MonoBehaviour
 {
+    //follow player
     public GameObject player;
-    public float speed;
+    public float speed = 1;
+
+
+    public int maxHealth = 3;
+    private int currentHealth;
 
 
  
     void Start()
     {
-        
+        currentHealth = maxHealth; 
     }
 
  
@@ -19,4 +24,28 @@ public class Enemy_AI : MonoBehaviour
     {
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
     }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        // Handle the mouse click on the enemy
+        TakeDamage(1); // adjust the damage
+    }
+
+    void Die()
+    {
+        //  any death logic here,  death animations, giving rewards, etc.
+        Destroy(gameObject);
+    }
+
+   
 }
