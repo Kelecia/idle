5c4a6c9eba832bbee1f084006f390e7d1a8f437f
keleciaa <1028416@student.sae.edu.au>
2023-11-02 18:02:31 +0800

    YES upgrade button now works and spawns and replaces old prefabs with the new

--- Assets/Scripts/UpgradeCacti.cs
+++ Assets/Scripts/UpgradeCacti.cs
@@ -4,55 +4,86 @@ using UnityEngine;
 
 public class UpgradeCacti : MonoBehaviour
 {
-    
-    
-        public CowSpawner cowSpawner;
-        public int upgradeCost = 2;
-        public GameObject upgradedPrefab;
-        private List<Transform> usedUpgradeSpawnPoints = new List<Transform>(); // List of used spawn points for upgraded objects
+    public CowSpawner cowSpawner;
+    public int upgradeCost = 2;
+    public GameObject upgradedPrefab;
+    private List<Transform> usedUpgradeSpawnPoints = new List<Transform>(); // List of used spawn points for upgraded objects
 
-        public string playerTag = "Player";
+    public string playerTag = "Player";
 
-        public void OnButtonClick()
+    private GameObject[] playersToUpgrade;
+    private int currentIndex = 0;
+    private bool isUpgrading = false;
+
+    public void OnButtonClick()
+    {
+        if (!isUpgrading && CoinCounter.instance.currentCoins >= upgradeCost)
         {
-            if (CoinCounter.instance.currentCoins >= upgradeCost)
+            CoinCounter.instance.IncreaseCoins(-upgradeCost); // Deduct the cost from the player's coins
+            playersToUpgrade = GameObject.FindGameObjectsWithTag(playerTag);
+            if (playersToUpgrade.Length > 0)
             {
-                CoinCounter.instance.IncreaseCoins(-upgradeCost); // Deduct the cost from the player's coins
-                ReplacePlayersWithUpgradedObject();
+                StartCoroutine(UpgradeNextPlayer());
             }
             else
             {
-                Debug.Log("Not enough coins to upgrade");
+                Debug.Log("No players to upgrade.");
             }
         }
-
-        private void ReplacePlayersWithUpgradedObject()
+        else
         {
-            GameObject[] players = GameObject.FindGameObjectsWithTag(playerTag);
-            if (cowSpawner.spawnPoints.Length > 0)
-            {
-                // Destroy the existing players
-                foreach (GameObject player in players)
-                {
-                    Destroy(player);
-                }
+            Debug.Log("Not enough coins to upgrade");
+        }
+    }
 
-                // Now, you can instantiate your upgraded game object in place of the players
-                // Ensure that upgraded objects do not spawn at the same location as other upgraded objects
-                foreach (Transform usedSpawnPoint in cowSpawner.usedSpawnPoints)
-                {
-                    if (!usedUpgradeSpawnPoints.Contains(usedSpawnPoint))
-                    {
-                        Vector3 spawnPosition = usedSpawnPoint.position;
-                        // Instantiate your upgraded game object here
-                        Instantiate(upgradedPrefab, spawnPosition, Quaternion.identity);
-                        usedUpgradeSpawnPoints.Add(usedSpawnPoint); // Mark the spawn point as used for upgraded objects
-                    }
-                }
+    private IEnumerator UpgradeNextPlayer()
+    {
+        isUpgrading = true;
+        GameObject playerToUpgrade = playersToUpgrade[currentIndex];
+        Transform spawnPoint = FindUnusedSpawnPoint();
+        if (spawnPoint != null)
+        {
+            Vector3 spawnPosition = spawnPoint.position;
+            // Instantiate your upgraded game object here
+            Instantiate(upgradedPrefab, spawnPosition, Quaternion.identity);
+            usedUpgradeSpawnPoints.Add(spawnPoint); // Mark the spawn point as used for upgraded objects
+
+            Destroy(playerToUpgrade); // Destroy the existing player
+
+            currentIndex++;
+
+            if (currentIndex >= playersToUpgrade.Length)
+            {
+                Debug.Log("All players have been upgraded.");
+                currentIndex = 0; // Reset the index for future upgrades
+            }
+
+            isUpgrading = false;
+        }
+        else
+        {
+            Debug.Log("No available spawn points for upgraded objects.");
+            isUpgrading = false;
+        }
+
+        yield return null;
+    }
+
+    private Transform FindUnusedSpawnPoint()
+    {
+        foreach (Transform usedSpawnPoint in cowSpawner.usedSpawnPoints)
+        {
+            if (!usedUpgradeSpawnPoints.Contains(usedSpawnPoint))
+            {
+                return usedSpawnPoint;
             }
         }
-    
+        return null;
+    }
 }
+
+
+
 
 
 
