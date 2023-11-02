130cd7c4368d2ce9356c8bedb12191acf56923a8
keleciaa <1028416@student.sae.edu.au>
2023-11-02 15:46:17 +0800

    zdsxfgv

--- Assets/Scripts/UpgradeCacti.cs
+++ Assets/Scripts/UpgradeCacti.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class UpgradeCacti : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnButtonClick()
     {
-        
+        if (CoinCounter.instance.currentCoins >= cowCost)
+        {
+            CoinCounter.instance.IncreaseCoins(-cowCost); // Deduct the cost from the player's coins
+            SpawnCow();
+        }
+        else
+        {
+            Debug.Log("Not enough coins to spawn a cowboy");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void SpawnCow()
     {
-        
+        if (spawnPoints.Length > 0)
+        {
+            List<Transform> availableSpawnPoints = new List<Transform>(spawnPoints);
+
+            // Remove used spawn points from the available spawn points list
+            foreach (Transform usedSpawnPoint in usedSpawnPoints)
+            {
+                availableSpawnPoints.Remove(usedSpawnPoint);
+            }
+
+            if (availableSpawnPoints.Count > 0)
+            {
+                int randomIndex = Random.Range(0, availableSpawnPoints.Count); // Choose a random spawn point
+                Transform spawnPoint = availableSpawnPoints[randomIndex];
+                usedSpawnPoints.Add(spawnPoint); // Mark the spawn point as used
+                Vector3 spawnPosition = spawnPoint.position;
+                Instantiate(cowPrefab, spawnPosition, Quaternion.identity);
+            }
+            else
+            {
+                Debug.Log("No available");
+            }
+
+
+        }
+
     }
+}
+
+
 }
