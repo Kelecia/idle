cf40cf91fbdde1f33196a0ec7ef54d47b878d9f4
keleciaa <1028416@student.sae.edu.au>
2023-11-02 17:42:16 +0800

    solved maybe

--- Assets/Scripts/UpgradeCacti.cs
+++ Assets/Scripts/UpgradeCacti.cs
@@ -4,55 +4,54 @@ using UnityEngine;
 
 public class UpgradeCacti : MonoBehaviour
 {
-    public CowSpawner cowSpawner;
-    public int upgradeCost = 2;
-    public GameObject upgradedPrefab;
-    private List<Transform> usedUpgradeSpawnPoints = new List<Transform>(); // List of used spawn points for upgraded objects
+    
+    
+        public CowSpawner cowSpawner;
+        public int upgradeCost = 2;
+        public GameObject upgradedPrefab;
+        private List<Transform> usedUpgradeSpawnPoints = new List<Transform>(); // List of used spawn points for upgraded objects
 
-    public string playerTag = "Player";
+        public string playerTag = "Player";
 
-
-    public void OnButtonClick()
-    {
-        if (CoinCounter.instance.currentCoins >= upgradeCost)
+        public void OnButtonClick()
         {
-            CoinCounter.instance.IncreaseCoins(-upgradeCost); // Deduct the cost from the player's coins
-            ReplaceCowsWithUpgradedObject();
-        }
-        else
-        {
-            Debug.Log("Not enough coins to upgrade");
-        }
-    }
-
-
-    private void ReplaceCowsWithUpgradedObject()
-    {
-        GameObject[] players = GameObject.FindGameObjectsWithTag(Player);
-        if (cowSpawner.spawnPoints.Length > 0)
-        {
-            // Destroy the existing cows
-            foreach (GameObject player in players)
+            if (CoinCounter.instance.currentCoins >= upgradeCost)
             {
-                Destroy(player);
+                CoinCounter.instance.IncreaseCoins(-upgradeCost); // Deduct the cost from the player's coins
+                ReplacePlayersWithUpgradedObject();
             }
-
-
-            // Now, you can instantiate your upgraded game object in place of the cows
-            // Ensure that upgraded objects do not spawn at the same location as other upgraded objects
-            foreach (Transform usedSpawnPoint in cowSpawner.usedSpawnPoints)
+            else
             {
-                if (!usedUpgradeSpawnPoints.Contains(usedSpawnPoint))
+                Debug.Log("Not enough coins to upgrade");
+            }
+        }
+
+        private void ReplacePlayersWithUpgradedObject()
+        {
+            GameObject[] players = GameObject.FindGameObjectsWithTag(playerTag);
+            if (cowSpawner.spawnPoints.Length > 0)
+            {
+                // Destroy the existing players
+                foreach (GameObject player in players)
                 {
-                    Vector3 spawnPosition = usedSpawnPoint.position;
-                    // Instantiate your upgraded game object here
-                    Instantiate(upgradedPrefab, spawnPosition, Quaternion.identity);
-                    usedUpgradeSpawnPoints.Add(usedSpawnPoint); // Mark the spawn point as used for upgraded objects
+                    Destroy(player);
                 }
 
+                // Now, you can instantiate your upgraded game object in place of the players
+                // Ensure that upgraded objects do not spawn at the same location as other upgraded objects
+                foreach (Transform usedSpawnPoint in cowSpawner.usedSpawnPoints)
+                {
+                    if (!usedUpgradeSpawnPoints.Contains(usedSpawnPoint))
+                    {
+                        Vector3 spawnPosition = usedSpawnPoint.position;
+                        // Instantiate your upgraded game object here
+                        Instantiate(upgradedPrefab, spawnPosition, Quaternion.identity);
+                        usedUpgradeSpawnPoints.Add(usedSpawnPoint); // Mark the spawn point as used for upgraded objects
+                    }
+                }
             }
         }
-    }
+    
 }
 
 
